447071c2f47c078fbaa2cb41d38d44365c1c7ab5
LugaXD <tyson.dzika@education.nsw.gov.au>
2018-05-15 15:52:42 +1000

    Finished off Breakout

--- Unity/Assets/~Breakout/Scripts/Ball.cs
+++ Unity/Assets/~Breakout/Scripts/Ball.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
+namespace Breakout
+{
+    public class Ball : MonoBehaviour
+    {
+        public float speed = 5f; // Speed that the ball travels
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private Vector3 velocity; // Velocity of the ball (Direction X Speed)
+
+        // Fires off ball in a given direction
+        public void Fire(Vector3 direction)
+        {
+            // Calculate velocity
+            velocity = direction * speed;
+        }
+
+        // Detect collisions
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            // Grab contact point of collision
+            ContactPoint2D contact = other.contacts[0];
+            // Calculate the reflection point of the ball using velocity & contact normal
+            Vector3 reflect = Vector3.Reflect(velocity, contact.normal);
+            // Calculate new velocity from reflection multiply by the same speed (velocity.magnitude)
+            velocity = reflect.normalized * velocity.magnitude;
+        }
+
+        private void Update()
+        {
+            // Moves ball using velocity & deltaTime
+            transform.position += velocity * Time.deltaTime;
+        }
+    }
 }
+
--- Unity/Assets/~Breakout/Scripts/Paddle.cs
+++ Unity/Assets/~Breakout/Scripts/Paddle.cs
@@ -2,15 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour {
+namespace Breakout
+{
+    public class Paddle : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+        public Ball currentBall;
+        // Directions array defaults to two values
+        public Vector2[] directions = new Vector2[]
+        {
+            new Vector2(-0.5f, 0.5f),
+            new Vector2(0.5f, 0.5f)
+        };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Use this for initialization
+        void Start()
+        {
+            // Grabs currentBall from children of the Paddle
+            currentBall = GetComponentInChildren<Ball>();
+        }
+
+        void Fire()
+        {
+            // Detach as child
+            currentBall.transform.SetParent(null);
+            // Generate random dir from lsit of directions
+            Vector3 randomDir = directions[Random.Range(0, directions.Length)];
+            // Fire off ball in randomDirection
+            currentBall.Fire(randomDir);
+        }
+        void CheckInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+
+        void Movement()
+        {
+            // Get input on the horizontal axis
+            float inputH = Input.GetAxis("Horizontal");
+            // Set force to direction (inputH to decide which direction)
+            Vector3 force = transform.right * inputH;
+            // Apply movement speed to force
+            force *= movementSpeed;
+            // Apply delta time to force
+            force *= Time.deltaTime;
+            // Add force to position
+            transform.position += force;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            CheckInput();
+            Movement();
+        }
+    }
 }

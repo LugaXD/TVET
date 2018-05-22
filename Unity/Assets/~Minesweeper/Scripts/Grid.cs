955883c30089a8948b1acd205df7123301b31555
LugaXD <tyson.dzika@education.nsw.gov.au>
2018-05-22 15:55:03 +1000

    Created ~Minesweeper. Added Tile generation

---
+++ Unity/Assets/~Minesweeper/Scripts/Grid.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MineSweeper
+{
+    public class Grid : MonoBehaviour
+    {
+        // Functions & Variables go here
+        public GameObject tilePrefab;
+        public int width = 10, height = 10;
+        public float spacing = .155f;
+
+        private Tile[,] tiles;
+
+        // Functionality for spawning tiles
+        Tile SpawnTile(Vector3 pos)
+        {
+            // Clone tile prefab
+            GameObject clone = Instantiate(tilePrefab);
+            // Edit it's properties
+            clone.transform.position = pos;
+            Tile currentTile = clone.GetComponent<Tile>();
+            // Return it
+            return currentTile;
+        }
+
+        // Spawns tiles in a grid like pattern
+        void GenerateTiles()
+        {
+            // Create a new 2D array of size width x height
+            tiles = new Tile[width, height];
+            // Loop through the entire tile list
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    // Store half size for later use
+                    Vector2 halfSize = new Vector2(width * 0.5f,
+                                                   height * 0.5f);
+                    // Pivot tiles around grid
+                    Vector2 pos = new Vector2(x - halfSize.x,
+                                              y - halfSize.y);
+                    pos += new Vector2(0.5f, 0.5f);
+                    // Apply spacing 
+                    pos *= spacing;
+                    Tile tile = SpawnTile(pos);
+                    tile.transform.SetParent(transform);
+                    tile.x = x;
+                    tile.y = y;
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+
+        void Start()
+        {
+            GenerateTiles();
+        }
+
+        public int GetAdjacentMineCount(Tile tile)
+        {
+            // Set count to 0
+            int count = 0;
+            // Loop through all adjacent tiles on the x
+            for (int x = -1; x <= 1; x++)
+            {
+                // Loop through all adjacent tile to look at
+                for (int y = -1; y <= 1; y++)
+                {
+                    // Calculate which adjacent tile to look at
+                    int desiredX = tile.x + x;
+                    int desiredY = tile.y + y;
+                    // Select current tile
+                    Tile currentTile = tiles[desiredX, desiredY];
+                    // Check if that tile is a mine
+                    if(currentTile.isMine)
+                    {
+                        // Increment count by 1
+                        count++;
+                    }
+                }
+            }
+
+            // Remember to return the count!
+            return count;
+        }
+    }
+}
---
+++ Unity/Assets/~Minesweeper/Scripts/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MineSweeper
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        // Functions & Variables go here
+        public int x, y;
+        public bool isMine = false; // Is the current tile a mine?
+        public bool isRevealed = false; // Has the tile already been revealed
+        [Header("References")]
+        public Sprite[] emptySprites; // List of empty sprites i.e, empty, 1, 2, 3, etc...
+        public Sprite[] mineSprites; // The mine sprites
+        private SpriteRenderer rend; // Reference to sprite renderer
+
+        void Awake()
+        {
+            // Grab reference to sprite renderer
+            rend = GetComponent<SpriteRenderer>();
+        }
+
+        void Start()
+        {
+            // Randomly decide if this title is a mine - using a 5% chance
+            isMine = Random.value < .05f;
+        }
+
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            // Flags the tile as being revealed
+            isRevealed = true;
+            // Check if tile is a mine
+            if(isMine)
+            {
+                // Sets sprite to mine sprite
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                // Sets sprite to appropriate texture bsaed on adjacent mines
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+    }
+}

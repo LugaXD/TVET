d77a100a1548359450e926c0a54dfcf08db21931
tyson.dzika1 <tyson.dzika1@600900J211DE415>
2018-11-13 15:39:18 +1100

    Im big noob and not know where to put my stuff got git cX

---
+++ TVETProjectMobile/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public List<GameObject> tiles = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ TVETProjectMobile/Assets/Scripts/Hazardscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazardscript : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(horizontalMove, verticalMove, 0) * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
